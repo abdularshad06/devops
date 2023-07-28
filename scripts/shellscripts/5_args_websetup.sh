d518a69fa36a766cf07978235a075989917c4fcd
abdularshad06 <abdularshad390@gmail.com>
2023-07-28 17:31:59 +0530

    Commit Changes

--- scripts/shellscripts/5_args_websetup.sh
+++ scripts/shellscripts/5_args_websetup.sh
@@ -1,49 +1,56 @@
 #!/bin/bash
 
+# Variable Declaration
+PACKAGE="httpd wget zip unzip"
+SVC="httpd"
+#URL="https://www.tooplate.com/zip-templates/2098_health.zip"
+#ART_NAME="2098_health"
+TEMPDIR="/tmp/webfiles"
+
 # Installing Dependencies
 echo "##############################################################"
 echo "Installing Packages."
 echo "##############################################################"
-sudo yum install wget unzip httpd -y > /dev/null
+sudo yum install $PACKAGE -y > /dev/null
 echo
 
 # Start & Enable Service
 echo "##############################################################"
 echo "Start & Enable Service"
 echo "##############################################################"
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo systemctl start $SVC
+sudo systemctl enable $SVC
 echo
 
 # Creating Temp Directory
 echo "##############################################################"
 echo "Starting Artifact Deployment"
 echo "##############################################################"
-mkdir -p /tmp/webfiles
-cd /tmp/webfiles
-wget https://www.tooplate.com/zip-templates/2098_health.zip > /dev/null
-unzip 2098_health.zip > /dev/null
-sudo cp -r 2098_health/* /var/www/html/
+mkdir -p $TEMPDIR
+cd $TEMPDIR
+wget $1 > /dev/null
+unzip $2.zip > /dev/null
+sudo cp -r $2/* /var/www/html/
 echo
 
 # Restart Httpd Service
 echo "##############################################################"
 echo "Restart Httpd Service"
 echo "##############################################################"
-sudo systemctl restart httpd
+sudo systemctl restart $SVC
 echo
 
 # Remove Temporary Files
 echo "##############################################################"
 echo "Remove Temporary Files"
 echo "##############################################################"
-rm -rf /tmp/webfiles
+rm -rf $TEMPDIR
 echo
 
 # Service Status and List root dir contents
 echo "##############################################################"
 echo "Service Status and List root dir contents"
 echo "##############################################################"
-systemctl status httpd
+systemctl status $SVC
 ls /var/www/html
 echo
